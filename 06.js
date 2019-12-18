38120d33297999f798019449887842eaaab8483d
Kostya <goncharovk911@gmail.com>
2019-12-18 15:57:22 +0200

    ClassWWW

---
+++ 06.js
@@ -0,0 +1,9 @@
+var whoClass = () => {
+  var element = document.getElementById("myunique");
+  if (element.classList.contains("www")) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+};
+whoClass();
