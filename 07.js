482a22e92d818a11a96c46c150360b058d576e6b
Kostya <goncharovk911@gmail.com>
2019-12-18 15:57:59 +0200

    DelAddClassWWW

---
+++ 07.js
@@ -0,0 +1,10 @@
+var whoClass = () => {
+  var element = document.getElementById("myunique");
+  element.classList.toggle("www");
+  /*if (element.classList.contains("www")) {
+    element.classList.remove("www")
+  } else {
+     element.classList.add("www")
+  }*/
+};
+whoClass();
